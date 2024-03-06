160c0cca80d6a3cd0c5e318cdd6ecf3cf62b875a
Zeugmarout <zeugmarout@gmail.com>
2024-03-05 19:31:52 -0800

    inching closer to working rivers. No array/backtracking or tests yet.

--- scripts/js/river_meander.js
+++ scripts/js/river_meander.js
@@ -1,76 +1,4 @@
-/* start the rivers meandering from the knots and orphans above
-    river_meander();
-        let right_left = right;
-        rotation = 0; // will be set by spawning knot/orphan.
-        // Rotation will be in increments of 60 degrees. CSS can transform accordingly, eg. "transform: rotate(45deg);"
-
-        roll for next segment to try - 
-                0.597826087 straight, 
-                0.3695652174 loose bend,        (0.9673913044 sum)
-                0.03260869565 sharp bend 
-            check to see if elevation allows this choice. If not re-roll. 
-            Woods allow a 0.08695652174 chance (8.7%) of river climbing 'up' in. 
-
-            Any bend alternates from right to left. All start right.
-                Squiggles and further knots (to join rivers that collide) simply added as needed, no proportion/chance.
-                Cosmetic tributaries (bifurcated straights) to be figured out once it's working.
-            Use another function to determine river illus based on entrance and exit faces.
-
-
-        
-
-
-    */
-
-// Global variables
-let right_turn = true;   // all rivers take a right turn to begin with, then alternate.
-
-function river_meander (prev_hex, exit_face_of_prev) {
-    //console.log(`River spawning from parent (previous) hex:`)
-    //console.log(prev_hex);
-    prev_anchor_id = prev_hex.id.slice(4);  // just the ID number
-    //console.log(`Previous anchor id: ${prev_anchor_id}`);
-
-    // test if find_adj works. This cannot be taken out or get_next_hex stops working below for some reason. 
-    //let neighbors = [];
-    //neighbors = find_adjacent(prev_anchor_id);
-    //console.log('Find adjacent returns:')
-    //console.log(neighbors);
-
-    //console.log(`Exit face from previous: ${exit_face_of_prev}`);
-    let this_hex = [];
-    this_hex = get_next_hex(prev_anchor_id, exit_face_of_prev);
-    //console.log('This river segment:')
-    //console.log(this_hex);
-
-    if (this_hex === null) {
-        return;
-    }
-    
-    // A function to merge rivers that collide into a knot
-    // Save entry and exit faces as hex data
-    // if this_hex has a waterway, combine the 2 existing faces with the incoming 3rd face. 
-    // Do some math to find the knot pattern (135, 145...) and rotate accordingly.
-    // return to terminate the river.
-
-    let river_illus = this_hex.querySelector('.hex-waterway');
-    let this_river_rotation = 0;
-
-    //console.log(`Spawning river. Exit face of knot: ${exit_face_of_prev}`);
-    let river_entry = exit_to_entry_face(exit_face_of_prev);
-    //console.log(`Entry face into first river hex: ${river_entry}`);
-    let river_exit = 0;
-
-
-    let river_type = 'straight';
-
-    let river_choice = Math.random();
-    if (river_choice <= 0.597826087) {
-        river_type = 'straight';
-        river_exit = straight_river_faces(river_entry);
-
-        /*
-
+/*
         let putative_next_hex = get_next_hex(this_hex, river_exit);  
         let suitable_for_rivers = river_tests(putative_next_hex);
         if(!suitable_for_rivers) { break out of this if statement and re-roll river_choice. Do this x times or start re-writing terrain.}
@@ -94,6 +22,68 @@ function river_meander (prev_hex, exit_face_of_prev) {
 
 
         */
+
+
+// Global variables
+let right_turn = true;   // all rivers take a right turn to begin with, then alternate.
+
+function river_meander (prev_hex, exit_face_of_prev) {
+    //console.log(`River spawning from parent (previous) hex:`)
+    //console.log(prev_hex);
+    prev_anchor_id = prev_hex.id.slice(4);  // just the ID number
+    //console.log(`Previous anchor id: ${prev_anchor_id}`);
+    //console.log(`Exit face from previous: ${exit_face_of_prev}`);
+    let this_hex = [];
+    this_hex = get_next_hex(prev_anchor_id, exit_face_of_prev);
+    //console.log('This river segment:')
+    //console.log(this_hex);
+    
+    if (this_hex === null) {
+        return;
+    }
+    
+    let neighbors = [];
+    neighbors = find_adjacent(this_hex.id.slice(4));
+    //console.log(this_hex);
+    //console.log('Find adjacent returns:')
+    //console.log(neighbors);
+
+    let river_illus = this_hex.querySelector('.hex-waterway');
+    let this_river_rotation = 0;
+
+    //console.log(`Spawning river. Exit face of knot: ${exit_face_of_prev}`);
+    let river_entry = exit_to_entry_face(exit_face_of_prev);
+    //console.log(`Entry face into first river hex: ${river_entry}`);
+    let river_exit = 0;
+    let river_type = 'undefined';
+
+    // CALL MEANDER FROM WITHIN ANOTHER FUNCTION, spawn_river, which is incharge of saving the meanders' hexes as an array.
+    // We must also be saving disallowed directions for the rivers as they go!  troubling... 
+
+    // has river entered a swamp? Chance to trail off in a Squiggle.
+    // check: has river crossed another? Form knot. 
+        // A function to merge rivers that collide into a knot
+        // Save entry and exit faces as hex data
+        // if this_hex has a waterway, combine the 2 existing faces with the incoming 3rd face. 
+        // Do some math to find the knot pattern (135, 145...) and rotate accordingly.
+        // return to terminate the river.
+
+    // checks complete. River is continuing as normal.    
+    // depending on right_turn, get 3 options for this_hex 
+        // use get_river_options, test to see that right_turn is in sync
+    // suitability tests phase: disqualify mountains, deserts, hexes adjacent to deserts...
+
+    // assign weights to the remaining choices. open/swamp gets the usual split, woods will get a small percentage (less than 8?) 
+    // random roll
+        
+    // assign illus, rotation
+
+    // post-
+
+    let river_choice = Math.random();
+    if (river_choice <= 0.597826087) {
+        river_type = 'straight';
+        river_exit = straight_river_faces(river_entry);
         river_illus.style.background = `url(../mats/Waterways/river_1-4_1.png)`;
 
     } else if (river_choice > 0.597826087 && river_choice <= 0.9673913044) {
@@ -182,6 +172,43 @@ function sharp_bend_right(entry) {
     }
     return exit;
 }
+
+
+function get_river_options (hex_id, river_entry) {
+    let options = Array();
+    
+    //straight
+    let straight_exit = straight_river_faces(river_entry);
+    let straight_hex = get_next_hex (hex_id, straight_exit);
+    console.log(straight_hex);
+    options.push(straight_hex);
+
+    //loose bend
+    if (right_turn) {
+        let loose_right_exit = loose_bend_right(river_entry);
+        let loose_right_hex = get_next_hex (hex_id, loose_right_exit);
+        console.log(loose_right_hex);
+        options.push(loose_right_hex);
+    } else {
+        let loose_left_exit = loose_bend_left(river_entry);
+        let loose_left_hex = get_next_hex (hex_id, loose_left_exit);
+        options.push(loose_left_hex);
+    }
+
+    //sharp bend
+    if (right_turn) {
+        let sharp_right_exit = sharp_bend_right(river_entry);
+        let sharp_right_hex = get_next_hex (hex_id, sharp_right_exit);
+        options.push(sharp_right_hex);
+    } else {
+        let sharp_left_exit = sharp_bend_left(river_entry);
+        let sharp_left_hex = get_next_hex (hex_id, sharp_left_exit);
+        options.push(sharp_left_hex);
+    }
+
+    return (options);
+}
+
 
 
 function river_tests(test_hex) {
